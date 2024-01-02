3d6ae1be974436093bec798d0f33c44f63bfe04d
xiasufan <sufanx@student.unimelb.edu.au>
2024-01-03 06:26:22 +1100

    Update index.js

--- client/src/components/checkLoginComponent/index.js
+++ client/src/components/checkLoginComponent/index.js
@@ -8,18 +8,23 @@ const useAuth = () => {
     const [loginRole, setLoginRole] = useState('');
 
     useEffect(() => {
-        Axios.get(`${process.env.REACT_APP_API_URL}/login`).then((response) => {
-          if (response.data.loggedIn == true) {
-            setLoginName(response.data.user[0].username)
-            setLoginRole (response.data.user[0].role)
-          }
-        });}, []);
+        const checkLoginStatus = async () => {
+            try {
+                const response = await Axios.get(`${process.env.REACT_APP_API_URL}/login`);
+                if (response.data.loggedIn) {
+                    setLoginName(response.data.user[0].username);
+                    setLoginRole(response.data.user[0].role);
+                }
+            } catch (error) {
+                console.error('login check failed:', error);
+                // 这里可以处理错误，如设置登录状态为未登录等
+            }
+        };
 
-        return { loginName, loginRole}
+        checkLoginStatus();
+    }, []); // 空依赖数组意味着效果仅在组件挂载时运行一次
 
-
-    
-
+    return { loginName, loginRole };
 };
 
 export default useAuth
